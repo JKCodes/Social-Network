0f3d65c985dd7a4ff3595724c19ce65abd082bf3
Joe Kim <josephkim0224@gmail.com>
2017-01-31 12:40:10 -0500

    Signin ui custom classes complete

---
+++ SocialNetwork/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  SocialNetwork
+//
+//  Created by Joseph Kim on 1/31/17.
+//  Copyright © 2017 Joseph Kim. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
