b5b717b4e9784a5fc35bcbdf2e0f90dd7cba52e5
Joe Kim <josephkim0224@gmail.com>
2017-02-02 14:21:38 -0500

    Likes finished

--- SocialNetwork/DataService.swift
+++ SocialNetwork/DataService.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import Firebase
+import SwiftKeychainWrapper
 
 // DATABASE_URL from GoogleService-Info.plist
 let DB_BASE = FIRDatabase.database().reference()
@@ -37,11 +38,19 @@ class DataService {
     var REF_USERS: FIRDatabaseReference {
         return _REF_USERS
     }
+
+    var REF_USER_CURRENT: FIRDatabaseReference {
+        let uid = KeychainWrapper.standard.string(forKey: KEY_UID)
+        let user = REF_USERS.child(uid!)
+        return user
+    }
+    
     
     var REF_POST_IMAGES: FIRStorageReference {
         return _REF_POST_IMAGES
     }
     
+   
     func createFirebaseDBUser(uid: String, userData: Dictionary<String, String>) {
         
         // Updates users data without overwriting existing data
